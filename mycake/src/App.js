680832bc17afa10689f8c6177bbafe76c7217ca4
yuhan <853860830@qq.com>
2020-07-14 00:23:59 +0800

    icon初步渲染ok
    icon初步渲染ok

--- mycake/src/App.js
+++ mycake/src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import { Button } from 'antd-mobile';
 // import './icon/iconfont/iconfont'
+// import './App.scss'
 function App() {
   return (
-    <Button>Start</Button>
+    <div className="container">
+      <Button>Start</Button>
+      <i className="iconfont icon-people icon1"></i>
+    </div>
   );
 }
 
--- mycake/src/index.js
+++ mycake/src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';
 // import { Button } from 'antd-mobile';
 import 'antd-mobile/dist/antd-mobile.css';  // or 'antd-mobile/dist/antd-mobile.less'
 import App from './App';
-import {icon} from 'antd-mobile'
-import './App.scss'
+// import {icon} from 'antd-mobile'
+// import './App.scss'
 
 ReactDOM.render(
   <React.StrictMode>
