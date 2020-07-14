27349c6ba6177c0b1a62b33e4cd470bd59e81360
yuhan <853860830@qq.com>
2020-07-14 21:11:44 +0800

    主页app.js
    主页app.js

--- mycake/src/App.js
+++ mycake/src/App.js
@@ -1,29 +1,50 @@
-import React from 'react';
+import React , { Component } from 'react';
 import { NavBar,Icon} from 'antd-mobile';
 import 'antd-mobile/dist/antd-mobile.css';
 // import './icon/iconfont/iconfont'
 import './App.css'
-function App() {
-  return (
-    <div className="container">
-      <NavBar
-        mode="light"
-        // icon={<i className="iconfont icon-title" ></i>}
-        leftContent={[
-          <i key="0" className="iconfont icon-title" ></i>,
-          <i key="1" className="city"></i>,
-          <span style={{ fontSize:'12px',color:'#442818' }}>广州</span>
-        ]}
-        onLeftClick={() => console.log('onLeftClick')}
-        rightContent={[
-          <i key="0" className="top-message" ></i>,
-          <i key="1" className="top-cart"></i>
-        ]}
-      >
-      <i className="iconfont icon-header-center" type="left" style={{ width:"30px" }}></i>
-    </NavBar>
-    </div>
-  );
-}
+import Home from './pages/Home/index';
+import Type from './pages/Type/index';
+import Person from './pages/Person/index'
 
-export default App;
+
+export default class App extends Component {
+  state={
+    current:'/home',
+    menu:[{
+      title:'首页',
+      path:'/home',
+      Component:Home
+    },{
+      title:'分类',
+      path:'/type',
+      Component:Type
+    },{
+      title:'个人',
+      path:'/person',
+      Component:Person
+    }]
+  }
+  render() {
+    return (
+      <div className="title-box">
+        <NavBar
+          mode="light"
+          // icon={<i className="iconfont icon-title" ></i>}
+          leftContent={[
+            <i key="0" className="iconfont icon-title" ></i>,
+            <i key="1" className="city"></i>,
+            <span key="2" style={{ fontSize:'12px',color:'#442818' }}>广州</span>
+          ]}
+          onLeftClick={() => console.log('onLeftClick')}
+          rightContent={[
+            <i key="0" className="top-message" ></i>,
+            <i key="1" className="top-cart"></i>
+          ]}
+        >
+        <i className="iconfont icon-header-center" type="left" style={{ width:"30px" }}></i>
+      </NavBar>
+      </div>
+    );
+  }
+}
---
+++ mycake/src/pages/Type/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Type extends Component {
+    render() {
+        return (
+            <div>
+                type
+            </div>
+        )
+    }
+}
