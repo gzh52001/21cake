bbfa97b77d8392caaeacb72f06d0ad5118860e62
yuhan <853860830@qq.com>
2020-07-14 19:46:47 +0800

    首页导航栏初步构建
    首页导航栏初步构建

--- mycake/src/App.js
+++ mycake/src/App.js
@@ -1,12 +1,26 @@
 import React from 'react';
-import { Button } from 'antd-mobile';
+import { NavBar,Icon} from 'antd-mobile';
+import 'antd-mobile/dist/antd-mobile.css';
 // import './icon/iconfont/iconfont'
-// import './App.scss'
+import './App.css'
 function App() {
   return (
     <div className="container">
-      <Button>Start</Button>
-      <i className="iconfont icon-people icon1"></i>
+      <NavBar
+        mode="light"
+        // icon={<i className="iconfont icon-title" ></i>}
+        leftContent={[
+          <i key="0" className="iconfont icon-title" ></i>,
+          <i key="1" className="city"></i>
+        ]}
+        onLeftClick={() => console.log('onLeftClick')}
+        rightContent={[
+          <Icon key="0" type="search" style={{ marginRight: '16px' }} />,
+          <Icon key="1" type="ellipsis" />,
+        ]}
+      >
+      <i className="iconfont icon-header-center" type="left"></i>
+    </NavBar>
     </div>
   );
 }
