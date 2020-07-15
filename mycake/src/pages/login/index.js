264ba2926756a32b595a2260d9cda1eb11a85433
xuliping <1493833791@qq.com>
2020-07-15 17:13:39 +0800

    静态注册页面初步完成

--- mycake/src/pages/login/index.js
+++ mycake/src/pages/login/index.js
@@ -7,9 +7,33 @@ class Login extends Comment{
     render(){
         return(
             <body>
-                
+                <div id="login-box">
+                    <h2>用户注册</h2>
+                    <ul>
+                        <li>
+                            <input class="" type="text" placeholder="输入手机号码"/>
+                            {/* <!-- 隐藏边框：style border：none --> */}
+                        </li>
+                        <li>
+                            <input class="" type="password" placeholder="密码：8～20字符，需同时包含英文和数字"/>
+                        </li>
+                        <li>
+                            <input class="" type="password" placeholder="确认密码"/>
+                        </li>
+                        <li>
+                            <input class="" type="text" placeholder="请选择生日"/>
+                        </li>
+                        <li>
+                            <button>注册</button>
+                        </li>
+                    </ul>
+                </div>
             </body>
         )
     }
 
 }
+
+
+
+export default Login;
