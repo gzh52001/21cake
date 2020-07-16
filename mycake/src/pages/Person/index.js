e1b4f5353b5531bad07f05ebddde0217d2eaf022
xuliping <1493833791@qq.com>
2020-07-16 17:51:59 +0800

    React格式修改

--- mycake/src/pages/Person/index.js
+++ mycake/src/pages/Person/index.js
@@ -4,67 +4,76 @@ import './person.css';
 import './iconfont.css';
 
 class Person extends Component {
+
+    phone = ()=>{
+        let phone = '13632945150';
+        console.log(phone);
+    }
     render() {
         return (
             <div>
-            <header class="personHeader">
+            <header className="personHeader">
             <h2>我的廿一客</h2>
         </header>
-        <div class="person_info">
-            <div class="per_logo">
+        <div className="person_info">
+            <div className="per_logo">
                 <img src="https://m.21cake.com/wap_themes/21cake/images/icon/ico_member.png" alt=""/>
             </div>
     
-            <ul class="per_msg">
+            <ul className="per_msg">
                 <li>21cake</li>
-                <li class="phone">13632945150</li>
-                <li class="changeMsg">
-                    <button>修改个人信息 &nbsp; {'>'} </button>
+                <li className="phone">13632945150</li>
+                <li className="changeMsg">
+                    <button onClick={this.phone}>修改个人信息 &nbsp; {'>'} </button>
                 </li>
             </ul>
     
-            <div class="per_pay">
-                <ul class="balance">
+            <div className="per_pay">
+                <ul className="balance">
                     <li>￥0.00</li>
                     <li>果实币余额</li>
                 </ul>
-                <ul class="coupon">
+                <ul className="coupon">
                     <li>0张</li>
                     <li>优惠券</li>
                 </ul>
             </div>
     
-            <ul class="per_list">
+            <ul className="per_list">
                 <li>
                     <a href="javascript:;">
-                        <span>待付订单</span><span class="iconfont iconiconfontjiantou5"></span>
+                        <span>待付订单</span><span className="iconfont iconiconfontjiantou5"></span>
                     </a>
                 </li>
                 <li>
                     <a href="javascript:;">
-                        <span>全部订单</span><span class="iconfont iconiconfontjiantou5"></span>
+                        <span>全部订单</span><span className="iconfont iconiconfontjiantou5"></span>
                     </a>
                 </li>
                 <li>
                     <a href="javascript:;">
-                        <span>地址管理</span><span class="iconfont iconiconfontjiantou5"></span>
+                        <span>地址管理</span><span className="iconfont iconiconfontjiantou5"></span>
                     </a>
                 </li>
             </ul>
-            <img class="per_img" src="https://static.21cake.com//upload/images/20200317/7649161442134eb493c5d60fbfe84540.jpeg" alt=""/>
+            <img className="per_img" src="https://static.21cake.com//upload/images/20200317/7649161442134eb493c5d60fbfe84540.jpeg" alt=""/>
     
-            <div class="per_exit">
+            <div className="per_exit">
                 <span>退出登录</span>
             </div>
     
-            <div class="per_footer">
+            <div className="per_footer">
                 <p>订购专线：&nbsp;&nbsp;400-650-2121</p>
                 <p>COPYRIGHT 2013  京ICP备06069127号  京公网安备 11011202001547</p>
             </div>
         </div>
         </div>
+        
         )
     }
+
+    
+
 }
 
 
--- mycake/src/pages/sign/index.js
+++ mycake/src/pages/sign/index.js
@@ -10,16 +10,16 @@ class Sign extends Comment{
                 <h2>手机号码登录</h2>
                 <ul>
                     <li>
-                         <input class="" type="text" placeholder="手机号码"/>
+                         <input className="" type="text" placeholder="手机号码"/>
                         {/* <!-- 隐藏边框：style border：none --> */}
                     </li>
                     <li>
-                        <input class="" type="password" placeholder="填写密码"/>
+                        <input className="" type="password" placeholder="填写密码"/>
                     </li>
                     <li>
                         <button>登录</button>
                     </li>
-                    <div class="gotoLogin">
+                    <div className="gotoLogin">
                         <a href="javascript:;">去注册</a>
                     </div>
                 </ul>
