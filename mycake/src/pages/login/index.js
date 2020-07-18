f78081a3309b4c94d0c192d34bff856e529e71d0
xuliping <1493833791@qq.com>
2020-07-18 11:45:19 +0800

    引入UI框架完成生日选择表单
    还需微调样式

--- mycake/src/pages/login/index.js
+++ mycake/src/pages/login/index.js
@@ -3,8 +3,53 @@ import React, { Component } from 'react';
 import './login.css';
 import './base.css';
 
+import 'antd-mobile/dist/antd-mobile.css';
+import { DatePicker, List } from 'antd-mobile';
+
+const nowTimeStamp = Date.now();
+const now = new Date(nowTimeStamp);
+// GMT is not currently observed in the UK. So use UTC now.
+const utcNow = new Date(now.getTime() + (now.getTimezoneOffset() * 60000));
+
+// Make sure that in `time` mode, the maxDate and minDate are within one day.
+let minDate = new Date(nowTimeStamp - 1e7);
+const maxDate = new Date(nowTimeStamp + 1e7);
+// console.log(minDate, maxDate);
+if (minDate.getDate() !== maxDate.getDate()) {
+  // set the minDate to the 0 of maxDate
+  minDate = new Date(maxDate.getFullYear(), maxDate.getMonth(), maxDate.getDate());
+}
+
+function formatDate(date) {
+  /* eslint no-confusing-arrow: 0 */
+  const pad = n => n < 10 ? `0${n}` : n;
+  const dateStr = `${date.getFullYear()}-${pad(date.getMonth() + 1)}-${pad(date.getDate())}`;
+  const timeStr = `${pad(date.getHours())}:${pad(date.getMinutes())}`;
+  return `${dateStr} ${timeStr}`;
+}
+
+// If not using `List.Item` as children
+// The `onClick / extra` props need to be processed within the component
+const CustomChildren = ({ extra, onClick, children }) => (
+  <div
+    onClick={onClick}
+    style={{ backgroundColor: '#fff', height: '45px', lineHeight: '45px', padding: '0 15px' }}
+  >
+    {children}
+    <span style={{ float: 'right', color: '#888' }}>{extra}</span>
+  </div>
+);
+
 class Login extends Component{
 
+    state = {
+        date: now,
+        time: now,
+        utcDate: utcNow,
+        dpValue: null,
+        customChildValue: null,
+        visible: false,
+      }
 
     err1 = ()=>{
         let phone = document.getElementsByClassName("phone")[0].value;
@@ -56,9 +101,29 @@ class Login extends Component{
                         <li>
                             <input className="pas2" onBlur={this.err3} type="password" placeholder="确认密码"/>
                         </li>
-                        <li>
+
+                        {/* <li>
                             <input className="" type="text" placeholder="请选择生日"/>
-                        </li>
+                        </li> */}
+
+                        <List className="date-picker-list" style={{ backgroundColor: 'white' }}>
+                            <DatePicker
+                            value={this.state.date}
+                            onChange={date => this.setState({ date })}
+                            >
+                            </DatePicker>
+                            <DatePicker
+                            mode="date"
+                            title="Select Date"
+                            extra="Optional"
+                            value={this.state.date}
+                            onChange={date => this.setState({ date })}
+                            minDate={new Date(1930, 1, 1, 23, 59, 59)}
+                            >
+                            <List.Item arrow="horizontal">请选择生日</List.Item>
+                            </DatePicker>
+                            </List>
+
                         <div className="erroBox">
                             <p className="errmsg-pho"><i></i> &nbsp; 请输入正确的手机号码</p>
                             <p className="errmsg-pas1"><i></i> &nbsp; 密码：8～20字符，需同时包含英文和数字</p>
@@ -77,7 +142,5 @@ class Login extends Component{
 
 
 }
-
-
 
 export default Login;
