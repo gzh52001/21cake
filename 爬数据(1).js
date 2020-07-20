93540eab48ebdc2a92216cc0f79277d46fd9e8ca
李彬 <996805301@qq.com>
2020-07-20 18:07:26 +0800

    更新一下爬的数据

--- 爬数据(1).js
+++ 爬数据(1).js
@@ -55,3 +55,28 @@ var arr=[]
 //    o.img=item.querySelector(".lazy").src;
 //    o.label=item.querySelector(".label img").src;
 //    console.log(o.label);
+
+//商品详情
+var arr=[]
+var data=document.querySelector(".swiper-container");
+var oDiv=data.querySelector(".swiper-wrapper").children;
+for(var i=0;i<oDiv.length;i++){
+    var o={};
+    var item=oDiv[i];
+    o.img=item.querySelector("img").src;
+   //  o.h3=item.querySelector("h3").innerText;
+   //  o.price=item.querySelector(".price").innerText;
+ //    o.label=item.querySelector(".label img");
+    if(item.querySelector("a img")){
+     o.label=item.querySelector("a img").src;
+    }
+ //    o.label=o.label.src
+ //未获取到label标签中的img链接
+    o.goodsId=i+999
+    arr.push(o)
+}
+
+//    console.log(arr[0].label.src);
+//    console.log(arr[0].img);
+// var arr=JSON.stringify(arr);
+console.log(arr);
