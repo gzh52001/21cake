de9c66dba0ea41eba99b3150e65cd21f75cd7b05
hosan-yip <56585077+hosan-yip@users.noreply.github.com>
2020-07-21 09:05:54 +0800

    面包屑

--- houtai/src/modules/home/pages/goodsMange/index.js
+++ houtai/src/modules/home/pages/goodsMange/index.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import { Breadcrumb  } from 'antd';
+import { HomeOutlined, UserOutlined } from '@ant-design/icons';
+
 
 function GoodsMange(){
     return (
         <div>
-            <h1>商品管理</h1>
+            <div>
+                <Breadcrumb>
+                    <Breadcrumb.Item href="">
+                    <HomeOutlined />
+                    </Breadcrumb.Item>
+                    <Breadcrumb.Item href="">
+                    <UserOutlined />
+                    <span>GoodsMange</span>
+                    </Breadcrumb.Item>
+                    <Breadcrumb.Item>商品管理</Breadcrumb.Item>
+                </Breadcrumb>
+            </div>
         </div>
     )
+    
 }
 
 export default GoodsMange;
