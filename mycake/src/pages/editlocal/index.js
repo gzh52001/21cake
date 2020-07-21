a188db4a418f369b5623a0e2e41f2305393f0c26
xuliping <1493833791@qq.com>
2020-07-21 12:05:57 +0800

    编辑地址页面完成

--- mycake/src/pages/editlocal/index.js
+++ mycake/src/pages/editlocal/index.js
@@ -1,9 +1,99 @@
 //地址编辑页面
 import React,{Component} from 'react';
 
+
 import './base.css'
 import './index.css'
 
+import { Cascader } from 'antd';
+import 'antd/dist/antd.css';
+
+const options = [
+    {
+      value: '广州市',
+      label: '广州市',
+      children: [
+        {
+            value: '白云区',
+            label: '白云区',
+        },
+        // {   
+        //     value: '广州大学城',
+        //     label: '广州大学城',
+        // },
+        // {   
+        //     value: '海珠区',
+        //     label: '海珠区',
+        // },
+        // {   
+        //     value: '黄浦区',
+        //     label: '黄浦区',
+        // },
+        // {   
+        //     value: '荔湾区',
+        //     label: '荔湾区',
+        // },
+        // {   
+        //     value: '南沙区',
+        //     label: '南沙区',
+        // },
+        {   
+            value: '番禺区',
+            label: '番禺区',
+        },
+        {   
+            value: '天河区',
+            label: '天河区',
+        },
+        {   
+            value: '越秀区',
+            label: '越秀区',
+        },
+        {   
+            value: '花都区',
+            label: '花都区',
+        },
+      ],
+    },
+    {
+      value: '深圳市',
+      label: '深圳市',
+      children: [
+        {
+          value: '宝安区',
+          label: '宝安区',
+        },
+        {
+          value: '福田区',
+          label: '福田区',
+        },
+        {
+          value: '南山区',
+          label: '南山区',
+        },
+        {
+          value: '龙岗区',
+          label: '龙岗区',
+        },
+        {
+          value: '罗湖区',
+          label: '罗湖区',
+        },
+        // {
+        //   value: '盐田区',
+        //   label: '盐田区',
+        // },
+        // {
+        //   value: '龙华区',
+        //   label: '龙华区',
+        // },
+      ],
+    },
+  ];
+
+  function onChange(value) {
+    console.log(value);
+  }
 class Editlocal extends Component{
 
     componentWillMount(){
@@ -37,6 +127,8 @@ class Editlocal extends Component{
         // this.goto.bind(null,'/sign');
     }
 
+    
+
     render(){
         return(
             <div className="box">
@@ -55,8 +147,13 @@ class Editlocal extends Component{
             </li>
             <li>
                 <label>城市</label>
-                <input type="text"/>
+                <Cascader options={options} onChange={onChange} placeholder="请选择城市" />
+                {/* <input type="text"/> */}
             </li>
+            {/* <Cascader/> */}
+
+            
+
             <li>
                 <label>地址</label>
                 <textarea name="" id="" cols="14" rows="6" placeholder="请填写小区/写字楼/学校等"></textarea>
