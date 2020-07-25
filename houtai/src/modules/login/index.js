5c8e461600d7f598f6d7d5f25e306dc2f4230264
xuliping <1493833791@qq.com>
2020-07-25 10:47:05 +0800

    登录功能完成

--- houtai/src/modules/login/index.js
+++ houtai/src/modules/login/index.js
@@ -5,10 +5,34 @@ import { UserOutlined, LockOutlined, SafetyOutlined } from '@ant-design/icons';
 
 import "antd/dist/antd.css"
 import "./index.scss"
+import "../../http"
 
 import Home from '../home'
+import http from '../../http';
 
 function Login(props) {
+
+    function login(){
+        //获取账号密码
+        let username = document.getElementsByClassName("ant-input")[0].value;
+        let password = document.getElementsByClassName("ant-input")[1].value;
+        
+        console.log(username,password);
+
+        http.get('/user/login',{username:username,psw:password}).then((res)=>{
+            console.log(res);
+            if(res.flag ==true){
+                setTimeout( alert("登录成功"), 1000);
+                props.history.push('/home');
+                
+            }else{
+                alert("登录失败，用户名或密码错误");
+            }
+        })
+
+        
+    }
+
     function onFinish(){
         props.history.push('/home'); 
     }
@@ -18,7 +42,7 @@ function Login(props) {
                 name="normal_login"
                 className="login-form"
                 initialValues={{ remember: true }}
-                onFinish={onFinish}
+                // onFinish={onFinish}
                 // onValuesChange={this.onCodeChange}
             >
                 <h1>21cake管理平台</h1>
@@ -38,20 +62,10 @@ function Login(props) {
                         placeholder="密码"
                     />
                 </Form.Item>
-                <Form.Item
-                    // name="captcha"
-                    rules={[{ required: false, message: '请输入验证码!' }]}
-                >
-                    <Input
-                        prefix={<SafetyOutlined className="site-form-item-icon" />}
-                        type="text"
-                        placeholder="验证码"
-                    />
-                    <img src="http://iph.href.lu/160x90?text=7985" />
-                </Form.Item>
+               
 
                 <Form.Item>
-                    <Button type="primary" htmlType="submit" className="login-form-button">
+                    <Button onClick={login} type="primary" htmlType="submit" className="login-form-button">
                         登录
                 </Button>
                 </Form.Item>
@@ -60,66 +74,6 @@ function Login(props) {
         </div>
     )
 }
-// class Login1 extends React.Component {
-//     constructor(props) {
-//         super(props);
-//         this.state = {
-//             number: 1
-//         }
-//         console.log(this.props)
-//     }
-//     onFinish() {
-//         // console.log(this);
-//     }
-//     render() {
-//         return (
-//             <div id="components-form-demo-normal-login">
-//                 <Form
-//                     name="normal_login"
-//                     className="login-form"
-//                     initialValues={{ remember: true }}
-//                     onFinish={this.onFinish}
-//                     onValuesChange={this.onCodeChange}
-//                 >
-//                     <h1>21cake管理平台</h1>
-//                     <Form.Item
-//                         name="username"
-//                         rules={[{ required: false, message: '请输入用户名!' }]}
-//                     >
-//                         <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="用户名" />
-//                     </Form.Item>
-//                     <Form.Item
-//                         name="password"
-//                         rules={[{ required: false, message: '请输入密码!' }]}
-//                     >
-//                         <Input.Password
-//                             prefix={<LockOutlined className="site-form-item-icon" />}
-//                             type="password"
-//                             placeholder="密码"
-//                         />
-//                     </Form.Item>
-//                     <Form.Item
-//                         // name="captcha"
-//                         rules={[{ required: false, message: '请输入验证码!' }]}
-//                     >
-//                         <Input
-//                             prefix={<SafetyOutlined className="site-form-item-icon" />}
-//                             type="text"
-//                             placeholder="验证码"
-//                         />
-//                         <img src="http://iph.href.lu/160x90?text=7985" />
-//                     </Form.Item>
 
-//                     <Form.Item>
-//                         <Button type="primary" htmlType="submit" className="login-form-button">
-//                             登录
-//                         </Button>
-//                     </Form.Item>
-//                 </Form>
-//                 <Route path='/home' component={Home} />
-//             </div>
-//         )
-//     }
-// }
 
 export default Login;
